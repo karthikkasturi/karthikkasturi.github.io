967315039292052a1d194e56d7215d15a486a358
Karthik Kasturi <karthikkasturi97@gmail.com>
2019-03-09 02:39:48 +0530

    JS + some css

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+console.log("Hello, jQuery!", $);
+
+isProduction = window.location.href == 'https://karthikkasturi.github.io'
+
+$(document).ready(function(){
+    setTimeout(function(){
+        if(isProduction) {
+            $('.production').show(); 
+            $('.dev').hide();
+        }
+        $(".loader").addClass("first-loaded").fadeOut();
+    }, 3000);
+    
+})
