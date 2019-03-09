7edc8dd07661e5f84499b24168a3118acb1364a4
Karthik Kasturi <karthikkasturi97@gmail.com>
2019-03-09 12:56:06 +0530

    Logo nightmare

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,23 @@ console.log("Hello, jQuery!", $);
 
 isProduction = window.location.href == 'https://karthikkasturi.github.io/'
 
-$(document).ready(function(){
-    setTimeout(function(){
-        if(isProduction) {
-            $('.production').show(); 
+// $.keyframe.define({
+//     name: 'logoReveal',
+//     '0%': {'margin-left': '-50px'},
+//     '100%': {'margin-left': '0px'},
+// })
+
+$(document).ready(function () {
+    setTimeout(function () {
+        if (isProduction) {
+            $('.production').show();
             $('.dev').hide();
         }
+        // $(selector).playKeyframe({
+        //     name: 'logoReveal',
+        //     duration: 2000,
+        //     timingFunction: 'linear',
+        //     });
         $(".loader").addClass("first-loaded").fadeOut();
     }, 3000);
-    
 })
